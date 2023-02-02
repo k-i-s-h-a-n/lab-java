4618ebbe5131ed6a623ee3ec1d4f22780d749eca
k-i-s-h-a-n <kishan86@gmail.com>
2023-02-02 08:40:22 +0530

    firstday(2/2/2023)

---
+++ JAVA_LEARNING/UserInput.java
@@ -0,0 +1,13 @@
+package JAVA_LEARNING;
+import java.util.Scanner;
+
+public class UserInput
+{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the value of n= ");
+        int n=scan.nextInt();
+        System.out.println(n);
+
+    }
+}
